e19a02b1b7b144a706071735d518b79608f6a1a3
Adilson Cruz <adilsondc1@hotmail.com>
2019-08-30 09:42:19 -0300

    adicionando os arquivo no git

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        
+    }
+
+    [Route("api/fornecedores")]
+    public  class FornecedoresController : MainController
+    {
+
+    }
+}
